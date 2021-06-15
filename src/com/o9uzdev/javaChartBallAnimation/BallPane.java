d260b61bab0b017e4dd7f08a56939c37a3f66197
o9uzdev <o9uzdev@gmail.com>
2021-06-15 12:39:17 +0300

    Added animation and moveBall method

--- src/com/o9uzdev/javaChartBallAnimation/BallPane.java
+++ src/com/o9uzdev/javaChartBallAnimation/BallPane.java
@@ -1,16 +1,41 @@
 package com.o9uzdev.javaChartBallAnimation;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import javafx.util.Duration;
 
 public class BallPane extends Pane {
     public final double radius = 20;
     private double x = radius, y = radius;
     private Circle circle = new Circle(x, y, radius);
+    private double dx = 1, dy = 1;
+    private Timeline animation;
 
     public BallPane() {
         circle.setFill(Color.BLUE); // Set ball color
         getChildren().add(circle); // Place a ball into this pane
+        // Create an animation for moving the ball
+        animation = new Timeline(new KeyFrame(Duration.millis(10), e -> moveBall()));
+        animation.setCycleCount(Timeline.INDEFINITE);
+        animation.play(); // Start animation
+    }
+
+    protected void moveBall() {
+        // Check boundaries
+        if (x < radius || x > getWidth() - radius) {
+            dx *= -1; // Change ball move direction
+        }
+        if (y < radius || y > getHeight() - radius) {
+            dy *= -1; // Change ball move direction
+        }
+
+        // Adjust ball position
+        x += dx;
+        y += dy;
+        circle.setCenterX(x);
+        circle.setCenterY(y);
     }
 }
