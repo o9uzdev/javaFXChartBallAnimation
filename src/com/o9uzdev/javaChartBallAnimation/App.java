7818a1a908798afd747e106230bde3d2c21ac7ca
o9uzdev <o9uzdev@gmail.com>
2021-06-16 11:05:44 +0300

    Added play pause animation methods

--- src/com/o9uzdev/javaChartBallAnimation/App.java
+++ src/com/o9uzdev/javaChartBallAnimation/App.java
@@ -4,14 +4,33 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class App extends Application {
+public class App extends Application{
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage primaryStage) {
         BallPane ballPane = new BallPane();
+
+        // Pause and resume animation
+        ballPane.setOnMousePressed(e -> ballPane.pause());
+        ballPane.setOnMouseReleased(e -> ballPane.play());
+
+        ballPane.setFocusTraversable(true);
+        ballPane.setOnKeyPressed(e -> {
+            switch (e.getCode()) {
+                case UP:
+                    ballPane.increaseSpeed();
+                    break;
+                case DOWN:
+                    ballPane.decreaseSpeed();
+                    break;
+            }
+        });
+
         // Create a scene and place it in the stage
-        Scene scene = new Scene(ballPane, 640, 480);
-        stage.setTitle("Ball Animation"); // Set the stage title
-        stage.setScene(scene); // Place the scene in the stage
-        stage.show(); // Display the stage
+        Scene scene = new Scene(ballPane, 250, 150);
+        primaryStage.setTitle("Ball Animation"); // Set the stage title
+        primaryStage.setScene(scene); // Place the scene in the stage
+        primaryStage.show(); // Display the stage
+
+        ballPane.requestFocus();
     }
 }
--- src/com/o9uzdev/javaChartBallAnimation/BallPane.java
+++ src/com/o9uzdev/javaChartBallAnimation/BallPane.java
@@ -2,6 +2,7 @@ package com.o9uzdev.javaChartBallAnimation;
 
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.beans.property.DoubleProperty;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -10,18 +11,40 @@ import javafx.util.Duration;
 public class BallPane extends Pane {
     public final double radius = 20;
     private double x = radius, y = radius;
-    private Circle circle = new Circle(x, y, radius);
     private double dx = 1, dy = 1;
+    private Circle circle = new Circle(x, y, radius);
     private Timeline animation;
 
     public BallPane() {
         circle.setFill(Color.BLUE); // Set ball color
         getChildren().add(circle); // Place a ball into this pane
+
         // Create an animation for moving the ball
-        animation = new Timeline(new KeyFrame(Duration.millis(10), e -> moveBall()));
+        animation = new Timeline(new KeyFrame(Duration.millis(50), e -> moveBall()));
         animation.setCycleCount(Timeline.INDEFINITE);
         animation.play(); // Start animation
     }
+    public void play() {
+        animation.play();
+    }
+
+    public void pause() {
+        animation.pause();
+    }
+
+    public void increaseSpeed() {
+        animation.setRate(animation.getRate() + 0.1);
+    }
+
+    public void decreaseSpeed() {
+        animation.setRate(
+                animation.getRate() > 0 ? animation.getRate() - 0.1 : 0);
+    }
+
+    public DoubleProperty rateProperty() {
+        return animation.rateProperty();
+    }
+
 
     protected void moveBall() {
         // Check boundaries
